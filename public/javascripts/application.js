587ce27fc1fa8429a0e17c43ad4fb9c4d5a9ede9
Felipe <felipejsilvacosta@gmail.com>
2020-10-23 11:25:41 +1100

    fix javascript Maths to display fee and total values

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,29 +1,37 @@
 let slider = document.getElementById("myRange");
-let output = document.querySelectorAll(".total");
+let totalValue = document.querySelectorAll(".total");
 let months = document.querySelectorAll(".months");
 let fee = document.querySelector(".fee");
 let totalRepayment = document.querySelector(".total-repayment");
 
-//  output[0].innerHTML = slider.value;
-//  output[1].innerHTML = slider.value;
-
-months.forEach(function (month) {
-  month.addEventListener("click", function (e) {
-    months.forEach((element) => {
-      element.classList.remove("months_active");
+months.forEach((month) => {
+  month.addEventListener("click", (e) => {
+    months.forEach((element) => element.classList.remove("months_active"));
+    e.currentTarget.classList.add("months_active");
+    current_fee = (Number(e.target.value) * Number(slider.value)) / 100;
+    totalValue.forEach((total_loan) => {
+      total_loan.innerHTML = slider.value;
+      fee.innerHTML = current_fee;
+      totalRepayment.innerHTML = current_fee + Number(slider.value);
     });
 
-    e.currentTarget.classList.add("months_active");
-    slider.oninput = function () {
-      output.forEach(function (value) {
-        current_range = document.getElementById("myRange");
-        current_value = document.querySelector(".total");
-        current_fee =
-          (Number(e.target.value) * Number(current_value.innerText)) / 100;
-        value.innerHTML = current_range.value; // updating the borrow wish
-        fee.innerHTML = current_fee; // update the fee
-        totalRepayment.innerHTML = current_fee + Number(current_range.value);
+    slider.oninput = () => {
+      current_fee = (Number(month.value) * Number(slider.value)) / 100;
+      totalValue.forEach((total_loan) => {
+        total_loan.innerHTML = slider.value;
+        fee.innerHTML = current_fee;
+        totalRepayment.innerHTML = this.current_fee + Number(slider.value);
       });
     };
   });
 });
+
+slider.oninput = () => {
+  debugger;
+  current_fee = (Number(months[0].value) * Number(slider.value)) / 100;
+  totalValue.forEach((total_loan) => {
+    total_loan.innerHTML = slider.value;
+    fee.innerHTML = current_fee;
+    totalRepayment.innerHTML = this.current_fee + Number(slider.value);
+  });
+};
